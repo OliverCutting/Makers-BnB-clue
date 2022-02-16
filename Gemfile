03310dc5412f4ae050275ffb0de7353a709a118c
Oliver Cutting <oliver.cutting@gmail.com>
2022-02-16 14:09:11 +0000

    removed simplecov

--- Gemfile
+++ Gemfile
@@ -14,6 +14,4 @@ gem 'bcrypt'
 group :test do
   gem 'capybara'
   gem 'rspec'
-  gem 'simplecov', require: false
-  gem 'simplecov-console', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,6 @@
 require 'capybara/rspec'
 require 'rspec'
-require 'simplecov'
-require 'simplecov-console'
 require_relative './setup_test_database'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-])
-SimpleCov.start
 
 ENV['RACK_ENV'] = 'test'
 ENV['ENVIRONMENT'] = 'test'
