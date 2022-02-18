2440d06d74c5fd2cfb8fc850d1362efa36edf275
Oliver Cutting <oliver.cutting@gmail.com>
2022-02-18 10:02:51 +0000

    SQL injection

--- lib/booking.rb
+++ lib/booking.rb
@@ -13,7 +13,7 @@ class Booking
   end
 
   def self.requests(owner_id)
-    result = DatabaseConnection.query("SELECT bookings.id, address, date, user_id, bookings.owner_id FROM bookings JOIN rooms ON (rooms.id=bookings.room_id) WHERE bookings.owner_id=#{owner_id}")
+    result = DatabaseConnection.query("SELECT bookings.id, address, date, user_id, bookings.owner_id FROM bookings JOIN rooms ON (rooms.id=bookings.room_id) WHERE bookings.owner_id=$1", ["#{owner_id}"])
     result.map do |booking|
     Booking.new(booking['id'], booking['address'], booking['date'], booking['user_id'], booking['owner_id'])
     end
